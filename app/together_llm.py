bf3f5955cbe140962a3ce4c2901bce59bae71b72
YashK-1 <college.yk@gmail.com>
2025-07-17 15:36:29 +0530

    Update together_llm.py

--- app/together_llm.py
+++ app/together_llm.py
@@ -1,12 +1,18 @@
 import requests
 import os
+import streamlit as st
 from dotenv import load_dotenv
+"""from dotenv import load_dotenv
 load_dotenv()
-key = os.getenv("TOGETHER_API_KEY")
+key = os.getenv("TOGETHER_API_KEY")"""
+# TOGETHER_API_KEY = st.secrets["TOGETHER_API_KEY"]
+load_dotenv()
+
+TOGETHER_API_KEY = st.secrets.get("TOGETHER_API_KEY", os.getenv("TOGETHER_API_KEY"))
 TOGETHER_API_URL = "https://api.together.xyz/v1/chat/completions"
 
 HEADERS = {
-    "Authorization": f"Bearer {key}",
+    "Authorization": f"Bearer {TOGETHER_API_KEY}",
     "Content-Type": "application/json"
 }
 
